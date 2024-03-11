44c93945c1635c49f7ef2e55cc4069fba3af074f
keerthi <01fe22bca209@kletech.ac.in>
2024-03-11 11:28:10 +0530

    changes made

---
+++ ExtendedOp.java
@@ -0,0 +1,10 @@
+public class ExtendedOp {
+    float modulus(float op1,float op2){
+        return(op1%op2);
+    }
+
+    double area_of_triangle(float base,float height){
+        return(0.5*base*height);
+    }
+ 
+}
